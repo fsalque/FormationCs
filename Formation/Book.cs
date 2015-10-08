5f837ae6deb3175c564a7115f2ab6772b34b11b7
fsalque <francois.salque@gmail.com>
2015-10-08 11:10:41 +0200

    Coucou

---
+++ Formation/Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formation
+{
+    class Book  
+    {
+        public string Title {
+            get; set;
+        }
+        public double Price {
+            get; set;
+        }
+        public string[] Text
+        {
+            get; set;
+        }
+        public int Id
+        {
+            get; set;
+        }
+        public double Evaluation
+        {
+            get; set;
+        }
+        public int NbPages
+        {
+            get; set;
+        }
+
+    }
+}
